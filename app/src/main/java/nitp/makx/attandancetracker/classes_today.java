0a21251e3580872026543ec25d858b9fe25063af
mayank-kr-verma <themayankverma@gmail.com>
2017-10-20 12:39:56 +0530

    First Commit

---
+++ app/src/main/java/nitp/makx/attandancetracker/classes_today.java
@@ -0,0 +1,15 @@
+package nitp.makx.attandancetracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class classes_today extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classes_today);
+    }
+}
